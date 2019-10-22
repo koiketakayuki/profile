8243476eb343130fa0d04228b7a85545fe4c6fdd
koiketakayuki <koiketakayuki.dev@gmail.com>
2019-10-23 01:06:14 +0900

    feat: add empty index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
-const Index = () => <h1>koiketakayuki's Page</h1>
+import React, { FunctionComponent } from 'react';
+import App from '../layouts/App';
+
+const Index: FunctionComponent = () => (
+  <App>
+    <h1>Profile</h1>
+  </App>
+);
 
 export default Index;
