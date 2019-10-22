3834206475814ed41443484381a294bb18ef0dd6
koiketakayuki <koiketakayuki.dev@gmail.com>
2019-10-23 00:13:44 +0900

    chore: update lint settings
    - install prettier
    - update lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,38 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
   extends: [
-    'airbnb',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+  plugins: ["react-hooks"],
+  rules: {
+    "prettier/prettier": ["error", { singleQuote: true }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
-  parser: '@typescript-eslint/parser',
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+    sourceType: "module"
+  }
 };
