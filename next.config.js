cab72a0e60755b9f727b226fe29ebb58aab5aaa8
koiketakayuki <koiketakayuki.dev@gmail.com>
2019-10-25 15:06:20 +0900

    chore: add GITHUB_READONLY_TOKEN env variable

--- next.config.js
+++ next.config.js
@@ -5,5 +5,10 @@ module.exports = {
       'schedule': { page: '/schedule' },
       'releases': { page: '/releases' }
     };
+  },
+  env: {
+    // GitHub token to be embedded in HTML
+    // No need to store this in secret because this isn't secret
+    GITHUB_READONLY_TOKEN: process.env.GITHUB_READONLY_TOKEN
   }
 };
