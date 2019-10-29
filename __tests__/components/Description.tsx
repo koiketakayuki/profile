6d47dd526f5a1999631540e9d9dcb81b0810c4bf
koiketakayuki <koiketakayuki.dev@gmail.com>
2019-10-30 02:01:18 +0900

    chore: move to components test directory

--- __tests__/components/Description.tsx
+++ __tests__/components/Description.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Description from '../components/Description';
+import Description from '../../components/Description';
 
 describe('<Description />', () => {
   it('shows label', async () => {
