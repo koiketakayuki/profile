4e730ca92ac14ff2c0c3ba855d8d335c10790e88
koiketakayuki <koiketakayuki.dev@gmail.com>
2019-10-30 00:59:08 +0900

    chore: config for collecting coverage

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,12 @@ module.exports = {
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.config.{js,ts}",
+    "!**/*.d.ts"
+  ],
+  coveragePathIgnorePatterns: ["coverage"]
 };
