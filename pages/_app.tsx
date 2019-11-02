c414084981697d4158fe0e17c39e2e65f62e7327
koiketakayuki <koiketakayuki.dev@gmail.com>
2019-11-02 09:19:57 +0900

    chore: fix page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ class MyApp extends App {
     return (
       <div className="app">
         <Head>
-          <title>koiketakayuk&#39;s Page</title>
+          <title>koiketakayuk&#39;s page</title>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
           <meta name="keywords" content="koiketakayuki,profile,engineer" />
